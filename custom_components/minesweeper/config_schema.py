c27059482f4ff2a4cc20e3f9c286e0cd6583572c
tejxhdx <77567865+tejxhdx@users.noreply.github.com>
2026-08-31 13:34:12 +0200

    Add config schema for hassfest

---
+++ custom_components/minesweeper/config_schema.py
@@ -0,0 +1,7 @@
+"""Configuration schema for the Minesweeper integration."""
+from __future__ import annotations
+
+import voluptuous as vol
+
+
+CONFIG_SCHEMA = vol.Schema({}, extra=vol.ALLOW_EXTRA)
